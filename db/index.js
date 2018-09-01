07337de074ecee9586a0a99e2c9b00109e816972
briantam23 <briantam23@gmail.com>
2018-09-01 18:02:29 -0400

    set up seeding

---
+++ db/index.js
@@ -0,0 +1,64 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme-user-thing-react');
+
+const User = conn.define('user', {
+    name: Sequelize.STRING
+})
+
+const Thing = conn.define('thing', {
+    name: Sequelize.STRING
+})
+
+const UserThing = conn.define('userThing', {
+    name: Sequelize.STRING
+})
+
+User.hasMany(UserThing);
+Thing.hasMany(UserThing);
+
+UserThing.belongsTo(User);
+UserThing.belongsTo(Thing);
+
+const syncAndSeed = () => {
+    let moe, larry, shep, joe, curly, foo1, foo2, foo3, bazz1, bazz2;
+    return conn.sync({ force: true })
+        .then(() => {
+            return Promise.all([
+                User.create({ name: 'moe' }),
+                User.create({ name: 'larry' }),
+                User.create({ name: 'shep' }),
+                User.create({ name: 'joe' }),
+                User.create({ name: 'curly' })
+            ]) 
+        })
+        .then((users) => {
+            [moe, larry, shep, joe, curly] = users;
+            return Promise.all([
+                Thing.create({ name: 'foo' }),
+                Thing.create({ name: 'foo' }),
+                Thing.create({ name: 'foo' }),
+                Thing.create({ name: 'bazz' }),
+                Thing.create({ name: 'bazz' })
+            ])
+        })
+        .then(things => {
+            [foo1, foo2, foo3, bazz1, bazz2] = things;
+            return Promise.all([
+                UserThing.create({ userId: moe.id, thingId: foo1.id }),
+                UserThing.create({ userId: moe.id, thingId: foo2.id }),
+                UserThing.create({ userId: larry.id, thingId: foo3.id }),
+                UserThing.create({ userId: larry.id, thingId: bazz1.id }),
+                UserThing.create({ userId: shep.id, thingId: bazz2.id })
+            ])
+        })
+}
+
+
+module.exports = {
+    models: {
+        User,
+        Thing,
+        UserThing
+    },
+    syncAndSeed
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+const db = require('./db');
+const { User, Thing, UserThing } = db.models;
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+app.listen(PORT, () => `listening on port ${PORT}`);
+
+app.use('/public', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/users', (req, res, next) => {
+    User.findAll({
+        include: [ UserThing ]
+    })
+        .then(users => res.send(users))
+        .catch(next);
+})
+
+
+
+db.syncAndSeed();
